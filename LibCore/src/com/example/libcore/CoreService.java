3f7d7cbd4405260b7c91fd9edd29bdb0411c5882
Mark Andrew <mark.andrew@gmail.com>
2014-05-03 14:42:06 +0200

    bulking up the service

--- LibCore/src/com/example/libcore/CoreService.java
+++ LibCore/src/com/example/libcore/CoreService.java
@@ -1,8 +1,11 @@
 package com.example.libcore;
 
+
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 
 public class CoreService extends Service {
 	public CoreService() {
@@ -12,5 +15,22 @@ public class CoreService extends Service {
 	public IBinder onBind(Intent intent) {
 		// TODO: Return the communication channel to the service.
 		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	@Override
+	public void onCreate(){
+		Log.i("foo","created service");
+	}
+	public void onDestroy(){
+		Log.i("foo","destroyed service");
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+
+		Bundle extras = intent.getExtras();
+		String source = extras.getString("source","none");
+
+		return Service.START_STICKY;
 	}
 }
