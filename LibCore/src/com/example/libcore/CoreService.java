527e75674e92ee9cf5a36a07fb526d32718a1c45
Mark Andrew <mark.andrew@gmail.com>
2014-05-03 16:12:40 +0200

    before the test GUI

--- LibCore/src/com/example/libcore/CoreService.java
+++ LibCore/src/com/example/libcore/CoreService.java
@@ -1,6 +1,7 @@
 package com.example.libcore;
 
 
+
 import com.example.libcommon.NestedMap;
 
 import android.app.Service;
@@ -11,6 +12,7 @@ import android.util.Log;
 
 public class CoreService extends Service implements Callout{
 	
+	static final String TAG = "com.example.libcore.CoreService";
 	Core core;
 	
 	public CoreService() {
@@ -19,20 +21,24 @@ public class CoreService extends Service implements Callout{
 	// ----------------- L i f e c y c l e
 	@Override
 	public void onCreate(){
-		Log.i("foo","created service");
+		Log.i(TAG,"created service");
 		core = Core.revive();
 		core.setCallout(this);
 	}
 	public void onDestroy(){
-		Log.i("foo","destroyed service");
+		Log.i(TAG,"destroyed service");
 		core.persist();
 	}
+	
 	// ----------------- D i s p a t c h   f r o m   i n t e n t s
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-
+		String actionName = intent.getAction();
 		Bundle extras = intent.getExtras();
-		String source = extras.getString("source","none");
+		String commandName = extras.getString("command");
+		
+		Log.i(TAG,"got intent with actionName:" + actionName + " commandName: " + commandName);
+		
 
 		return Service.START_STICKY;
 	}
@@ -40,6 +46,8 @@ public class CoreService extends Service implements Callout{
 	
 	
 	// ----------------- C a l l o u t
+	
+	// TODO Intent building and sending utility here
 	
 	@Override
 	public void showProgress(int progress) {
