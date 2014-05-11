1ea61daaaa29b56e67a4e2a35c7472d795f2bce3
Mark Andrew <mark.andrew@gmail.com>
2014-05-11 12:08:13 +0200

    deal with activity death-by-reconfiguration

--- XdevCore/src/com/example/xdevcore/MainActivity.java
+++ XdevCore/src/com/example/xdevcore/MainActivity.java
@@ -50,6 +50,12 @@ public class MainActivity extends Activity {
 		super.onResume();
 	}
 
+	
+	@Override protected void onDestroy() {
+		super.onDestroy();
+		unregisterReceiver(bReceiver);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
